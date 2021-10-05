4c3e262d461edba76145bf0f4988032a8b344e9e
Mahamurugan01 <91820831+Mahamurugan01@users.noreply.github.com>
2021-10-05 20:31:29 +0530

    Update checkPrimePgm.java
    prime _Updated correct PGM

--- checkPrimePgm.java
+++ checkPrimePgm.java
@@ -1,25 +1,21 @@
 package assignmentsweek1;
 
-import org.omg.Messaging.SyncScopeHelper;
-
 public class checkPrimePgm {
 
 	public static void main(String[] args) {
-int input = 13;
-boolean flag=false;
-for (int i = 2; i <= input; i++) {
-	
-	
-		break;
-		
-}
-if(flag==true)
-	{
-	System.out.println("Prime");
-	}
-else
-System.out.println("NotPrime");
-}
+		int input = 13;
+		boolean flag = false;
+		for (int i = 2; i < input; i++) {
+			if (input % i == 0) {
+				flag = true;
+				break;
+			}}
 
-}}
+			if (flag == false) {
+				System.out.println("Prime");
+			} else
+				System.out.println("NotPrime");
+		}
+
+	}
 
